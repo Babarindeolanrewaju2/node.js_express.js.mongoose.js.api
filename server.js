4bea5726a1d4a61dc381af9cd77c5d054ca9a679
Babarindeolanrewaju2 <babarindeolanrewaju@gmail.com>
2020-07-05 15:30:35 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var expres = requrie('express');
+var app = express();
+
+app.get('/', function (request, response) {
+    response.send('My First API');
+})
+
+app.listen(3000, function () {
+    console.log("First API running on port 3000!")
+})
