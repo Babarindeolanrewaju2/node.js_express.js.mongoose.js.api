dee0328f27a567d820f7e3e98eed93b939032cf5
Babarindeolanrewaju2 <babarindeolanrewaju@gmail.com>
2020-07-05 16:19:54 +0100

    third commit

--- server.js
+++ server.js
@@ -1,69 +1,87 @@
-var expres = requrie('express');
+var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/swag-shop');
+
+var Product = require('./model/product');
+var WishList = require('./model/wishlist');
+
+//Allow all requests from all domains & localhost
+app.all('/*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET");
+    next();
+});
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var ingredients = [
-    {
-        "id": "232kAk",
-        "text": "Eggs"
-    },
-    {
-        "id": "dkP345",
-        "text": "Milk"
-    },
-    {
-        "id": "dkcuu7",
-        "text": "Bacon"
-    },
-    {
-        "id": "73hdy",
-        "text": "Frogs Legs"
-    }
-];
-
-app.get('/', function (request, response) {
-    response.send(ingredients);
-})
-
-app.post('/ingredients', function (request, response) {
-    var ingredient = request.body;
-    if (!ingredient || ingredient.text === "") {
-        response.status(500).send({ error: "Your ingredient must have text" });
-    } else {
-        ingredients.push(ingredient);
-        response.status(200).send(ingredients);
-    }
-});
-
-app.put('/ingredients/:ingredientId', function (request, response) {
-
-    var newText = request.body.text;
-
-    if (!newText || newText === "") {
-        response.status(500).send({ error: "You must provide ingredient text" })
-    } else {
-        var objectFound = false;
-        for (var x = 0; x < ingredients.length; x++) {
-            var ing = ingredients[x];
-
-            if (ing.id === request.params.ingredientId) {
-                ingredients[x].text = newText;
-                objectFound = true;
-                break;
-            }
-        }
-
-        if (!objectFound) {
-            response.status(500).send({ error: "Ingredient id not found" });
+app.post('/product', function (request, response) {
+    var product = new Product();
+    product.title = request.body.title;
+    product.price = request.body.price;
+    product.save(function (err, savedProduct) {
+        if (err) {
+            response.status(500).send({ error: "Could not save product" });
         } else {
-            response.send(ingredients);
+            response.send(savedProduct);
         }
-    }
+    });
 });
 
-app.listen(3000, function () {
-    console.log("First API running on port 3000!");
+app.get('/product', function (request, response) {
+
+    Product.find({}, function (err, products) {
+        if (err) {
+            response.status(500).send({ error: "Could not fetch products" });
+        } else {
+            response.send(products);
+        }
+    });
+});
+
+app.get('/wishlist', function (request, response) {
+    WishList.find({}).populate({ path: 'products', model: 'Product' }).exec(function (err, wishLists) {
+        if (err) {
+            response.status(500).send({ error: "Could not fetch wishlists" });
+        } else {
+            response.status(200).send(wishLists);
+        }
+    })
+});
+
+app.post('/wishlist', function (request, response) {
+    var wishList = new WishList();
+    wishList.title = request.body.title;
+
+    wishList.save(function (err, newWishList) {
+        if (err) {
+            response.status(500).send({ error: "Could not create wishlist" });
+        } else {
+            response.send(newWishList);
+        }
+    });
+});
+
+app.put('/wishlist/product/add', function (request, response) {
+    Product.findOne({ _id: request.body.productId }, function (err, product) {
+        if (err) {
+            response.status(500).send({ error: "Could not add item to wishlist" });
+        } else {
+            WishList.update({ _id: request.body.wishListId }, { $addToSet: { products: product._id } }, function (err, wishList) {
+                if (err) {
+                    response.status(500).send({ error: "Could not add item to wishlist" });
+                } else {
+                    response.send("Successfully added to wishlist");
+                }
+            });
+        }
+    })
+});
+
+app.listen(3004, function () {
+    console.log("Swag Shop API running on port 3004...");
 });
